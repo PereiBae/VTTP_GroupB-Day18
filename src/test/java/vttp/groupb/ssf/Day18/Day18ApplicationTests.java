fcceacd384fb78e9ce8bfdb74ea7a3c10963d1d4
PereiBae <pereibae@gmail.com>
2024-12-02 09:47:10 +0800

    First commit

---
+++ src/test/java/vttp/groupb/ssf/Day18/Day18ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.groupb.ssf.Day18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
